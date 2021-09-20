4dc74048a56749eca4f2ce67b9b1288909e1c80c
Hamid.Bazargan <h.bazargan@gmail.com>
2021-09-20 11:02:36 +0430

    Update demo app.

--- app/src/main/java/ir/cafebazaar/referrersdksample/MainActivity.kt
+++ app/src/main/java/ir/cafebazaar/referrersdksample/MainActivity.kt
@@ -2,9 +2,12 @@ package ir.cafebazaar.referrersdksample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import ir.cafebazaar.referrersdk.ReferrerClient
 import ir.cafebazaar.referrersdk.ReferrerStateListener
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,47 +18,40 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         ReferrerClient.newBuilder(applicationContext).build().apply {
-            startConnection(object : ReferrerStateListener {
-                override fun onReferrerSetupFinished(referrerResponse: Int) {
-                    when (referrerResponse) {
-                        ReferrerClient.OK -> {
-                            referrer?.let {
-                                Toast.makeText(
-                                    applicationContext,
-                                    "REFERRER:$it",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                consumeReferrer(it.installBeginTimestampMilliseconds)
+            GlobalScope.launch {
+                startConnection(object : ReferrerStateListener {
+                    override fun onReferrerSetupFinished(referrerResponse: Int) {
+                        when (referrerResponse) {
+                            ReferrerClient.OK -> {
+                                referrer?.let {
+                                    showMessage("REFERRER:${it.referrer}")
+                                    consumeReferrer(it.installBeginTimestampMilliseconds)
+                                } ?: kotlin.run {
+                                    showMessage("REFERRER: THERE IS NO REFERRER")
+                                }
+                            }
+                            ReferrerClient.DEVELOPER_ERROR -> {
+                                showMessage("REFERRER:DEVELOPER_ERROR")
+                            }
+                            ReferrerClient.SERVICE_UNAVAILABLE -> {
+                                showMessage("REFERRER:SERVICE_UNAVAILABLE")
                             }
                         }
-                        ReferrerClient.DEVELOPER_ERROR -> {
-                            Toast.makeText(
-                                applicationContext,
-                                "REFERRER:DEVELOPER_ERROR",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                        ReferrerClient.FEATURE_NOT_SUPPORTED -> {
-                            Toast.makeText(
-                                applicationContext,
-                                "REFERRER:FEATURE_NOT_SUPPORTED",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                        ReferrerClient.SERVICE_UNAVAILABLE -> {
-                            Toast.makeText(
-                                applicationContext,
-                                "REFERRER:SERVICE_UNAVAILABLE",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
                     }
-                }
 
-                override fun onReferrerServiceDisconnected() {
-                    endConnection()
-                }
-            })
+                    override fun onReferrerServiceDisconnected() {
+                        endConnection()
+                    }
+                })
+            }
+        }
+    }
+
+    private fun showMessage(message: String) {
+        with(findViewById<TextView>(R.id.textview)) {
+            post {
+                text = message
+            }
         }
     }
 }
