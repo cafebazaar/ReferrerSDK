370f4d9f0ae6b00e20e764797c347afcb55d7a88
Hamid.Bazargan <h.bazargan@gmail.com>
2021-09-21 11:31:37 +0430

    Update the demo app.

--- app/src/main/java/ir/cafebazaar/referrersdksample/MainActivity.kt
+++ app/src/main/java/ir/cafebazaar/referrersdksample/MainActivity.kt
@@ -6,9 +6,13 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import ir.cafebazaar.referrersdk.ReferrerClient
+import ir.cafebazaar.referrersdk.ReferrerDetails
 import ir.cafebazaar.referrersdk.ReferrerStateListener
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,24 +22,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        showError("")
         ReferrerClient.newBuilder(applicationContext).build().apply {
-            lifecycleScope.launch {
+            lifecycleScope.launch(Dispatchers.IO) {
                 startConnection(object : ReferrerStateListener {
                     override fun onReferrerSetupFinished(referrerResponse: Int) {
                         when (referrerResponse) {
                             ReferrerClient.OK -> {
                                 referrer?.let {
-                                    showMessage("REFERRER:${it.referrer}")
+                                    showMessages(it)
                                     consumeReferrer(it.installBeginTimestampMilliseconds)
                                 } ?: kotlin.run {
-                                    showMessage("REFERRER: THERE IS NO REFERRER")
+                                    showError("THERE IS NO REFERRER")
                                 }
                             }
                             ReferrerClient.DEVELOPER_ERROR -> {
-                                showMessage("REFERRER:DEVELOPER_ERROR")
+                                showError("DEVELOPER_ERROR")
                             }
                             ReferrerClient.SERVICE_UNAVAILABLE -> {
-                                showMessage("REFERRER:SERVICE_UNAVAILABLE")
+                                showError("SERVICE_UNAVAILABLE")
                             }
                         }
                     }
@@ -48,11 +53,38 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showMessage(message: String) {
-        with(findViewById<TextView>(R.id.textview)) {
+    private fun showMessages(referrerDetails: ReferrerDetails) {
+        with(findViewById<TextView>(R.id.clicktime)) {
+            post {
+                text = referrerDetails.referrerClickTimestampMilliseconds.millisecondsToTime()
+            }
+        }
+        with(findViewById<TextView>(R.id.installtime)) {
+            post {
+                text = referrerDetails.installBeginTimestampMilliseconds.millisecondsToTime()
+            }
+        }
+        with(findViewById<TextView>(R.id.version)) {
+            post {
+                text = referrerDetails.appVersion.toString()
+            }
+        }
+        with(findViewById<TextView>(R.id.content)) {
+            post {
+                text = referrerDetails.referrer.toString()
+            }
+        }
+    }
+
+    private fun showError(message: String) {
+        with(findViewById<TextView>(R.id.error)) {
             post {
                 text = message
             }
         }
+    }
+
+    private fun Long.millisecondsToTime(): String {
+        return Date(this).toString()
     }
 }
