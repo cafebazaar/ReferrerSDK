f516a335d587266507bd7ec73e429a0151639d60
Hamid.Bazargan <h.bazargan@gmail.com>
2021-09-29 11:28:21 +0330

    Add javadoc for startConnection function in ReferrerClient.kt

--- referrersdk/src/main/java/ir/cafebazaar/referrersdk/ReferrerClient.kt
+++ referrersdk/src/main/java/ir/cafebazaar/referrersdk/ReferrerClient.kt
@@ -3,6 +3,12 @@ package ir.cafebazaar.referrersdk
 import android.content.Context
 
 abstract class ReferrerClient: ClientState() {
+    /**
+     * Establishes the connection between SDK and Bazaar client.
+     * @param stateListener A listener which user can use it to get the state of the connection.
+     * @throws Exception You have to call this function off the main thread
+     * otherwise it throws an exception.
+     */
     abstract suspend fun startConnection(stateListener: ReferrerStateListener)
     abstract fun endConnection()
     abstract val isReady: Boolean
